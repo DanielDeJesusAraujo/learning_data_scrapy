40f532cc1e88bbfac69401e799ee292727b1c99b
DanielDeJesusAraujo <danielaraljo115@gmail.com>
2023-10-02 18:59:33 -0300

    feat: scraping data

--- scrapy/books.py
+++ scrapy/books.py
@@ -3,44 +3,41 @@ import parsel
 import os
 
 status = {
-    'maped_urls': '>>> mapped urls 0',
-    'scraping_data': '>>> scraping data: 0.0%',
+    "maped_urls": ">>> mapped urls 0",
+    "scraping_data": ">>> scraping data: 0.0%",
 }
 
 
 def show_status():
-    os.system('clear')
-    print(status['maped_urls'])
-    print(status['scraping_data'])
+    os.system("clear")
+    print(status["maped_urls"])
+    print(status["scraping_data"])
 
 
 def getThumbsUrls(url: str):
     # print('>>> getThumbsUrls')
     response = requests.get(url)
     selector = parsel.Selector(text=response.text)
-    artcles_urls = selector.css('.image_container a::attr(href)').getall()
-    buttonNext = selector.css('.next a::attr(href)').get()
+    artcles_urls = selector.css(".image_container a::attr(href)").getall()
+    buttonNext = selector.css(".next a::attr(href)").get()
 
-    return {
-        'artcles_urls': artcles_urls,
-        'nextUrl': buttonNext
-    }
+    return {"artcles_urls": artcles_urls, "nextUrl": buttonNext}
 
 
 def getAllThumbUrls():
     # print('>>> getAllThumbUrls')
     allThumbs = []
-    page = 'https://books.toscrape.com/catalogue/page-1.html'
+    page = "https://books.toscrape.com/catalogue/page-1.html"
     hasNextPage = True
 
     while hasNextPage:
         # print(page)
         response = getThumbsUrls(page)
-        allThumbs.extend(response['artcles_urls'])
+        allThumbs.extend(response["artcles_urls"])
 
-        status['maped_urls'] = f'>>> mapped urls {len(allThumbs)}'
+        status["maped_urls"] = f">>> mapped urls {len(allThumbs)}"
         show_status()
-        if response['nextUrl'] == None:
+        if response["nextUrl"] == None:
             hasNextPage = False
         page = f'https://books.toscrape.com/catalogue/{response["nextUrl"]}'
     return allThumbs
@@ -50,12 +47,12 @@ def getBookInfo(url):
     # print('>>> getBookInfo')
     book_page = requests.get(url)
     selector = parsel.Selector(text=book_page.text)
-    title = selector.css('.product_main h1::text').get()
-    price = selector.css('.product_main p.price_color::text').get()
+    title = selector.css(".product_main h1::text").get()
+    price = selector.css(".product_main p.price_color::text").get()
 
     return {
-        'title': title,
-        'price': price,
+        "title": title,
+        "price": price,
     }
 
 
@@ -65,10 +62,15 @@ def getAllBookInfos():
     allBooksInfos = []
 
     for index in range(len(booksUrl)):
-        bookInfo = getBookInfo(f'https://books.toscrape.com/catalogue/{booksUrl[index]}')
+        bookInfo = getBookInfo(
+            f"https://books.toscrape.com/catalogue/{booksUrl[index]}"
+        )
         allBooksInfos.extend(bookInfo)
-        status['scraping_data'] = f'>>> scraping data: {(index * 100) / len(booksUrl)}%'
+        status["scraping_data"] = (
+            f">>> scraping data: {(index * 100) / len(booksUrl)}%"
+        )
         show_status()
+    return allBooksInfos
 
 
 getAllBookInfos()
